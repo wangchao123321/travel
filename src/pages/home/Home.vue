1947f1340c55bff2d738e51d2d8a82857055aed9
wangchao <244067166@qq.com>
2018-08-28 23:55:53 +0800

    1

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,13 +1,13 @@
 <template>
-  <div id="app">
-    <router-view/>
+  <div>
+    hello word
   </div>
 </template>
 
 <script>
-export default {
-  name: 'App'
-}
+  export default {
+    name: "Home"
+  }
 </script>
 
 <style>
